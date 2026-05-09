c085f63fca44192939761f3517fb1ccef75aa953
Luca Knobel <knobel.luca@gmail.com>
2026-05-09 21:15:34 +0200

    feat: add settings page and update navigation with settings link

---
+++ app/pages/settings.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth-middleware'],
+  layout: 'app'
+})
+</script>
+
+<template>
+  <div class="space-y-6">
+    WIP - settings page coming soon!
+  </div>
+</template>
